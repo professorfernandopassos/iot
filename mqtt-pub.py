bb57592d03cbdaefe2a59da026fc78e4e241d02b
Fernando Passos <fernando@pop-os.localdomain>
2021-10-01 19:56:19 -0300

    mqtt completo

--- mqtt-pub.py
+++ mqtt-pub.py
@@ -6,3 +6,29 @@ PORT = 1883
 client = mqtt.Client('pubpy')
 client.connect(BROKER, PORT)
 
+def on_publish_callback(client, userdata, result):
+    print("Dado publicado")
+
+client.on_publish = on_publish_callback
+
+while(True):
+    value = input('Digite 1 para ligado ou 0 para desligado: ')
+    status = "false"
+    if(value == "0"):
+        status = "false"
+    elif(value == "1"):
+        status = "true"
+    client.publish('casa/sala/lampada/1','{ "status": %s }' % str(status))
+    print('{ "status": %s }' % str(status))
+    # value = input('Digite 1 para ligado ou 0 para desligado: ')
+    # status = False
+    # if(value == "0"):
+    #     status = False
+    # elif(value == "1"):
+    #     status = True
+    # client.publish('casa/sala/lampada/1','{ "status": %s }' % str(status).lower())
+    # print('{ "status": %s }' % str(status).lower())
+    
+
+
+
--- mqtt-sub.py
+++ mqtt-sub.py
@@ -1,18 +1,37 @@
 import paho.mqtt.client as mqtt
 import time
+import json
 
 client = mqtt.Client(client_id='devpy')
 
 client.connect('localhost', port=1883)
 client.subscribe('casa/sala/lampada/1')
+# client.subscribe('casa/sala/lampada/1/intensidade')
 
-print("Subscriber connected")
+print("Subscriber connected!")
 
 def on_message_callback(client, userdata, message):
-    print(message.payload.decode('utf-8'))
+    # print(message.payload.decode('utf-8'))
+    msg = json.loads(message.payload.decode('utf-8'))
+    if(msg["status"] == True):
+        # Liga pino do raspberry / arduino
+        print("Pino X ligado")
+    elif(msg["status"] == False):
+        # Desliga pino do raspberry / arduino
+        print("Pino X desligado")
+
+    # if(msg["value"]):
+        # Lampada na intensidade x
+
+
+
+
+    # print(message.payload.decode('utf-8'))
 
 client.on_message = on_message_callback
 
 client.loop_forever()
+# client.loop_start()
 # time.sleep(100)
 # client.loop_stop()
+
